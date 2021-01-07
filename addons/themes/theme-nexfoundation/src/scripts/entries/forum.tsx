d51a728bfc3dbcf9ac873f64e9b7219ef635dac4
york <york@ruten.co.jp>
2021-01-07 17:45:48 +0800

    update forum.tsx

--- addons/themes/theme-nexfoundation/src/scripts/entries/forum.tsx
+++ addons/themes/theme-nexfoundation/src/scripts/entries/forum.tsx
@@ -8,14 +8,42 @@ import React from "react";
 import { Advertisement } from "../components/advertisement";
 import { onContent } from "@vanilla/library/src/scripts/utility/appUtils";
 import { mountReact } from "@vanilla/react-utils";
+import ReactDOM from "react-dom";
 
 onContent(() => {
     bootstrap();
+    articleList();
 });
 
 function bootstrap() {
     const adElement = document.getElementById("nex-advertisement");
     if (adElement) {
         mountReact(<Advertisement />, adElement, undefined);
+    }
+}
+function articleList() {
+    var ItemDiscussionLength = document.getElementsByClassName("ItemDiscussion").length + 1;
+
+    for (let i = 1; i < ItemDiscussionLength; i++) {
+        var element = document.getElementById(`Discussion_${i}`);
+        var json = {};
+        json.data = JSON.parse(element.dataset.meta).tags;
+        json.id = i;
+
+        let menuItems = [];
+        for (var g = 0; g < json.data.length; g++) {
+            menuItems.push(<div class="tag">{`#${json.data[g].name}`}</div>);
+        }
+
+        var adElement = document.getElementById(`tag_${i}`);
+        if (adElement) {
+            mountReact(
+                <div>
+                    <div class="tagBlock">{menuItems}</div>
+                </div>,
+                adElement,
+                undefined,
+            );
+        }
     }
 }
