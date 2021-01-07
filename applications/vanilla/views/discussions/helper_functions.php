c41d83f4187b1755168ae45e0dec0c73e3f0923a
york <york@ruten.co.jp>
2021-01-07 18:12:18 +0800

    Update article list UI components

--- applications/vanilla/views/discussions/helper_functions.php
+++ applications/vanilla/views/discussions/helper_functions.php
@@ -220,6 +220,12 @@ if (!function_exists('WriteDiscussion')) :
    </span>
 
             <div class="ItemContent Discussion">
+                <div class="tag"  aria-level="3" id="tag_<?php echo $discussion->DiscussionID; ?>">
+                <?php
+                // echo ($discussion->DataAttribute);
+                // echo ($discussion->Bookmarked);
+                ?>
+                </div>
                 <div class="Title" role="heading" aria-level="3">
                     <?php
                     echo adminCheck($discussion, ['', ' ']).anchor($discussionName, $discussionUrl);
@@ -233,11 +239,21 @@ if (!function_exists('WriteDiscussion')) :
                     <?php
                     writeTags($discussion);
                     ?>
-                    <span class="MItem MCount ViewCount"><?php
-                        printf(pluralTranslate($discussion->CountViews,
-                            '%s view html', '%s views html', t('%s view'), t('%s views')),
-                            bigPlural($discussion->CountViews, '%s view'));
+                    <span class="MItem MCount ViewCount">
+                    <svg width="15" height="20" viewBox="0 0 15 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.119 0.25C12.6369 0.25 13.1335 0.478273 13.4996 0.884602C13.8658 1.29093 14.0715 1.84203 14.0715 2.41667V19.75L7.23786 16.5L0.404221 19.75V2.41667C0.404221 1.84203 0.609927 1.29093 0.976086 0.884602C1.34224 0.478273 1.83886 0.25 2.35669 0.25H12.119ZM6.26163 4.58333V6.75H4.30916V8.91667H6.26163V11.0833H8.2141V8.91667H10.1666V6.75H8.2141V4.58333H6.26163Z" fill="#FF3559"/>
+</svg>
+
+                    <?php
+
+
+                        echo($discussion->Bookmarked);
+                        echo( t('bookmarked'));
                         ?></span>
+                        <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.55914 20C7.29393 20 7.03957 19.8946 6.85204 19.7071C6.6645 19.5196 6.55914 19.2652 6.55914 19C6.55914 17.3431 5.216 16 3.55914 16H2.55914C2.02871 16 1.52 15.7893 1.14493 15.4142C0.769857 15.0391 0.559143 14.5304 0.559143 14V2C0.559143 1.46957 0.769857 0.960859 1.14493 0.585786C1.52 0.210714 2.02871 0 2.55914 0H18.5591C19.0896 0 19.5983 0.210714 19.9734 0.585786C20.3484 0.960859 20.5591 1.46957 20.5591 2V14C20.5591 14.5304 20.3484 15.0391 19.9734 15.4142C19.5983 15.7893 19.0896 16 18.5591 16H17.6988C14.3444 16 11.1279 17.3349 8.75914 19.71C8.55914 19.9 8.30914 20 8.05914 20H7.55914ZM14.5591 9C15.1114 9 15.5591 8.55229 15.5591 8C15.5591 7.44772 15.1114 7 14.5591 7C14.0069 7 13.5591 7.44772 13.5591 8C13.5591 8.55229 14.0069 9 14.5591 9ZM10.5591 9C11.1114 9 11.5591 8.55229 11.5591 8C11.5591 7.44772 11.1114 7 10.5591 7C10.0069 7 9.55914 7.44772 9.55914 8C9.55914 8.55229 10.0069 9 10.5591 9ZM6.55914 9C7.11143 9 7.55914 8.55229 7.55914 8C7.55914 7.44772 7.11143 7 6.55914 7C6.00686 7 5.55914 7.44772 5.55914 8C5.55914 8.55229 6.00686 9 6.55914 9Z" fill="#FF3559"/>
+</svg>
+
          <span class="MItem MCount CommentCount"><?php
              printf(pluralTranslate($discussion->CountComments,
                  '%s comment html', '%s comments html', t('%s comment'), t('%s comments')),
