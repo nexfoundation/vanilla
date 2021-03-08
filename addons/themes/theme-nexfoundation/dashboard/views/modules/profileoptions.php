13e2b816af87b2c6326a913f712e237761e11db1
Benno <blueworrybear@gmail.com>
2021-03-08 22:24:29 +0800

    Merge dogfood (#85)
    * feat: customize the view
    
    * style: tags
    
    * fix lint
    
    * fix: panel css
    
    * hide nav bar
    
    * update #46: change img extension
    
    * update #65: hide activity and inbox
    
    * update #65: hide profile options
    
    Co-authored-by: Sheng Yang Liu <shengyangliu@Shengs-MacBook-Pro.local>
    Co-authored-by: jeffsyliu <51170697+jeffsyliu@users.noreply.github.com>
    Co-authored-by: Quentin. Kun-Yu Tsai <quentin.tsai@gmail.com>

---
+++ addons/themes/theme-nexfoundation/dashboard/views/modules/profileoptions.php
@@ -0,0 +1,5 @@
+<?php if (!defined('APPLICATION')) exit();
+/** Displays the "Edit My Profile" or "Back to Profile" buttons on the top of the profile page. */
+?>
+<div>
+</div>
---
+++ addons/themes/theme-nexfoundation/views/modules/categories.php
@@ -0,0 +1,60 @@
+<?php if (!defined('APPLICATION')) exit();
+$CountDiscussions = 0;
+$CategoryID = isset($this->_Sender->CategoryID) ? $this->_Sender->CategoryID : '';
+$OnCategories = strtolower($this->_Sender->ControllerName) == 'categoriescontroller' && !is_numeric($CategoryID);
+if ($this->Data !== FALSE) {
+    foreach ($this->Data->result() as $Category) {
+        $CountDiscussions = $CountDiscussions + $Category->CountDiscussions;
+    }
+    ?>
+    <div class="Box BoxCategories">
+        <?php echo '<div class="hot-forum">'.t('Categories').'</div>'; ?>
+        <div class="hot-forum-root">
+            <?php
+            if (!Gdn::themeFeatures()->useDataDrivenTheme()) {
+                echo '<li'.($OnCategories ? ' class="Active"' : '').'>'.
+                    anchor('<span class="Aside"><span class="Count">'.bigPlural($CountDiscussions, '%s discussion').'</span></span> '.t('All Categories'), '/categories', 'ItemLink ItemLinkAllCategories')
+                    .'</li>';
+            }
+
+            $MaxDepth = c('Vanilla.Categories.MaxDisplayDepth');
+
+            foreach ($this->Data->result() as $Category) {
+                if ($Category->CategoryID < 0 || $MaxDepth > 0 && $Category->Depth > $MaxDepth)
+                    continue;
+
+                $attributes = false;
+
+                if ($Category->DisplayAs === 'Heading') {
+
+                    $CssClass = 'Heading '.$Category->CssClass;
+                    $attributes = ['aria-level' => $Category->Depth + 2];
+                } else {
+                    $CssClass = 'Depth'.$Category->Depth.($CategoryID == $Category->CategoryID ? ' Active' : '').' '.$Category->CssClass;
+                }
+
+
+                if (is_array($attributes)) {
+                    $attributes = attribute($attributes);
+                }
+
+                echo '<div class="hot-forum-root_topic">';
+
+                if ($Category->CountAllDiscussions > 0) {
+                    $CountText = '<span class="Aside"><span class="Count">'.bigPlural($Category->CountAllDiscussions, '%s discussion').'</span></span>';
+                } else {
+                    $CountText = '';
+                }
+
+                if ($Category->DisplayAs === 'Heading') {
+                    echo $CountText.' '.htmlspecialchars($Category->Name);
+                } else {
+                    echo anchor($CountText.' '.htmlspecialchars($Category->Name), categoryUrl($Category), 'ItemLink');
+                }
+                echo "</div>\n";
+            }
+            ?>
+        </div>
+    </div>
+<?php
+}
